24e2f7dcd16e1b1d200c27515df94c959f7524b0
Sit <nasu.deniska@mail.ru>
2020-02-24 20:54:54 +0200

    initial commit

---
+++ Switch/kek.java
@@ -0,0 +1,16 @@
+public class kek {
+  public static void main(String[] args) {
+
+char a='a';
+if(a == 'a' && a == 'e' && a == 'i' && a == 'o'){System.out.println("vocala - a");}
+
+
+char b='b';
+switch(b){
+case 'b':System.out.println("consana - b"); break;
+case 'c':System.out.println("consana - c"); break;
+case 'd':System.out.println("consana - d"); break;
+}
+
+}
+}
---
+++ dowhile/kek.java
@@ -0,0 +1,14 @@
+public class kek {
+  public static void main(String[] args) {
+    int n = 50;
+    int i = 0;
+    int rs = 0;
+    
+    while (i < n){
+rs += i;
+i++;
+}  
+
+System.out.println(rs);
+  }
+}
